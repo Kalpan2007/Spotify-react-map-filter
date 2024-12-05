df230a9fa28b1cf992ec3b481875d6191a54884a
Kalpan2007 <kalpankaneriya@gmail.com>
2024-12-05 16:52:29 +0530

    using filert

--- src/content/content.jsx
+++ src/content/content.jsx
@@ -1,32 +1,26 @@
-import './content.css'
-
+import './content.css';
 
 function Content() {
-
-
     const data = [
-
-        {id: 1, title: 'Liked Songs', img_url: '../../public/Liked.png'},
-        {id: 2, title: 'Neffex Playlist', img_url: '../../public/image4.png'},
-        {id: 3, title: 'K/DA', img_url: '../../public/image3.png'},
-        {id: 4, title: 'Liked Songs', img_url: '../../public/image1.png'},
-
-    
-];
-
-
-
-    const datatwo = [
-        {id: 1, title: 'Liked Songs', img_url: '../../public/image9.png'},
-        {id: 2, title: 'Liked Songs2', img_url: '../../public/image8.png'},
-        {id: 3, title: 'Liked Songs3', img_url: '../../public/image7.png'},
-        {id: 4, title: 'Liked Songs', img_url: '../../public/image6.png'},
-        {id: 5, title: 'Liked Songs', img_url: '../../public/image5.png'},
+        { id: 1, title: 'Liked Songs fake', img_url: '../../public/image1.png' },
+        { id: 2, title: 'Liked Songs', img_url: '../../public/Liked.png' },
+        { id: 3, title: 'Neffex Playlist fake', img_url: '../../public/image10.png' },
+        { id: 4, title: 'Neffex Playlist', img_url: '../../public/image4.png' },
+        { id: 5, title: 'K/DA fake', img_url: '../../public/image8.png' },
+        { id: 6, title: 'K/DA', img_url: '../../public/image3.png' },
+        { id: 8, title: 'Liked Songs', img_url: '../../public/image1.png' },
     ];
 
+    const datatwo = [
+        { id: 1, title: 'Liked Songs', img_url: '../../public/image9.png' },
+        { id: 2, title: 'Liked Songs2', img_url: '../../public/image8.png' },
+        { id: 3, title: 'Liked Songs3', img_url: '../../public/image7.png' },
+        { id: 4, title: 'Liked Songs', img_url: '../../public/image6.png' },
+        { id: 5, title: 'Liked Songs', img_url: '../../public/image5.png' },
+    ];
 
-
-
+    
+    const a = data.filter((item) => item.id % 2 == 0);
 
     return (
         <>
@@ -37,22 +31,21 @@ function Content() {
                 </div>
                 <h1 className="secrowh">Good Morning</h1>
                 <div className="secondrow">
-
-                <div className="liked-songs-container">
-                        {data.map((item) => (
+                   
+                    <div className="liked-songs-container">
+                        {a.map((item) => (
                             <div key={item.id} className="liked-song-item">
                                 <img src={item.img_url} alt={item.title} className="liked-song-image" />
                                 <div className="liked-song-text">{item.title}</div>
                             </div>
                         ))}
                     </div>
-
                 </div>
                 <div className="thirdrow">
-                    <div class="liked-songs-container">
-                        <div class="liked-song-item">
-                            <img src="../public/image2.png" alt="Liked Songs" class="liked-song-image" />
-                            <p class="liked-song-text">Liked Songs</p>
+                    <div className="liked-songs-container">
+                        <div className="liked-song-item">
+                            <img src="../public/image2.png" alt="Liked Songs" className="liked-song-image" />
+                            <p className="liked-song-text">Liked Songs</p>
                         </div>
                     </div>
                 </div>
@@ -60,16 +53,14 @@ function Content() {
                 <div className="fourthrow">
                     {datatwo.map((k) => (
                         <div key={k.id} className="box-1">
-                            <img src={k.img_url} alt={k.title}  />
+                            <img src={k.img_url} alt={k.title} />
                             <div>{k.title}</div>
                         </div>
                     ))}
-                      
-                  
                 </div>
             </div>
         </>
-    )
+    );
 }
 
 export default Content;
